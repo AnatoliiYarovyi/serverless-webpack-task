766bdc0ecf29fb80863d97d300d9d26b253f350b
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-04-07 13:53:14 +0300

    Adds "middlewareEdytResponse"

--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -1,6 +1,28 @@
-import middy from "@middy/core"
-import middyJsonBodyParser from "@middy/http-json-body-parser"
+import middy from '@middy/core';
+import middyJsonBodyParser from '@middy/http-json-body-parser';
+import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
-export const middyfy = (handler) => {
-  return middy(handler).use(middyJsonBodyParser())
-}
+const middlewareEdytResponse = (): middy.MiddlewareObj<
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult
+> => {
+  const after: middy.MiddlewareFn<
+    APIGatewayProxyEvent,
+    APIGatewayProxyResult
+  > = async (request): Promise<void> => {
+    const { statusCode, body } = request.response;
+
+    request.response = {
+      statusCode,
+      body: JSON.stringify(body),
+    };
+  };
+
+  return { after };
+};
+
+export const middyfy = handler => {
+  return middy(handler)
+    .use(middyJsonBodyParser())
+    .use(middlewareEdytResponse());
+};
